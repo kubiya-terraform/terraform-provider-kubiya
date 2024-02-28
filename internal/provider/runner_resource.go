ae8767863db6ac673ca8c546f52edea6cd0128af
mevrat.avraham <mevrat.avraham@kubiya.ai>
2024-02-28 12:19:10 +0200

    provider

--- internal/provider/runner_resource.go
+++ internal/provider/runner_resource.go
@@ -3,12 +3,11 @@ package provider
 import (
 	"context"
 	"fmt"
-	"regexp"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"terraform-provider-kubiya/internal/clients"
+	"terraform-provider-kubiya/internal/resource_agent"
 	"terraform-provider-kubiya/internal/resource_runner"
 )
 
@@ -25,40 +24,31 @@ func NewRunnerResource() resource.Resource {
 	return &runnerResource{}
 }
 
-func (r *runnerResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data resource_runner.RunnerModel
-
-	// Read Terraform prior state data into the model
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// Read API call logic
-
-	// Save updated data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+func (r *runnerResource) Read(_ context.Context, _ resource.ReadRequest, _ *resource.ReadResponse) {
+	//var data resource_runner.RunnerModel
+	//
+	//// Read Terraform prior state data into the model
+	//resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	//
+	//if resp.Diagnostics.HasError() {
+	//	return
+	//}
+	//
+	//// Read API call logic
+	//
+	//// Save updated data into Terraform state
+	//resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *runnerResource) Update(_ context.Context, _ resource.UpdateRequest, _ *resource.UpdateResponse) {
 }
 
 func (r *runnerResource) Schema(ctx context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = resource_runner.RunnerResourceSchema(ctx)
+	resp.Schema = resource_agent.AgentResourceSchema(ctx)
 }
 
 func (r *runnerResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	var data resource_runner.RunnerModel
-
-	var validator = func(name string) bool {
-		// Updated the regex pattern to enforce a minimum length of 5 characters
-		pattern := `^[a-z][a-z0-9-]{4,}[a-z]$`
-		regex := regexp.MustCompile(pattern)
-
-		// Test the input string against the pattern
-		return regex.MatchString(name)
-	}
 
 	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
@@ -69,37 +59,17 @@ func (r *runnerResource) Create(ctx context.Context, req resource.CreateRequest,
 
 	// Create API call logic
 	name := data.Name.ValueString()
-	if valid := validator(name); !valid {
-		resp.Diagnostics.AddError(
-			"Name is not valid name",
-			"Runner name can only contain lowercase alpha-numeric characters. Spaces and underscores are not allowed.",
-		)
-		return
-	}
-
-	path := data.RunnerDeploymentFolder.ValueString()
-	if data.RunnerDeploymentFolder.IsNull() ||
-		data.RunnerDeploymentFolder.IsUnknown() {
-		resp.Diagnostics.AddError(
-			"filepath is missing or empty",
-			"filepath is missing or empty. ex. /Users/mevrat.avraham/, /Users/mevrat.avraham/runners/",
-		)
-		return
-	}
-
-	runner, err := r.client.CreateRunner(name, path)
+	runner, err := r.client.CreateRunner(name)
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"failed to create runner",
-			"failed to create runner. Error: "+err.Error(),
+			"failed to create runner"+err.Error(),
 		)
 		return
 	}
 
-	data.Url = types.StringValue(runner.Url)
-	data.RunnerDeploymentFile = types.StringValue(runner.Path)
 	// Save data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &runner)...)
 }
 
 func (r *runnerResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
@@ -118,7 +88,7 @@ func (r *runnerResource) Delete(ctx context.Context, req resource.DeleteRequest,
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"failed to delete runner",
-			"failed to delete runner. Error: "+err.Error(),
+			"failed to delete runner"+err.Error(),
 		)
 	}
 }
