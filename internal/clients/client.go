b8f6b03bf0629efab77d3284582b94f1a81b80bb
mevrat.avraham <mevrat.avraham@kubiya.ai>
2024-04-30 20:02:12 +0300

    * feature: split client to files by resource type

--- internal/clients/client.go
+++ internal/clients/client.go
@@ -1,10 +1,11 @@
 package clients
 
 import (
-	"encoding/json"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -15,256 +16,68 @@ const (
 )
 
 type Client struct {
-	host    string
-	userKey string
-	client  *http.Client
+	host         string
+	email        string
+	userKey      string
+	organization string
+	client       *http.Client
 }
 
-func NewClient(key string) (*Client, error) {
-	if len(key) >= 1 {
+func NewClient(k, e, o string) (*Client, error) {
+	if len(k) >= 1 {
 		host := defaultHost
 		timeout := defaultTimeout
 		client := &http.Client{Timeout: timeout}
-		return &Client{host: host, userKey: key, client: client}, nil
+		return &Client{host: host, email: e,
+			userKey: k, organization: o, client: client}, nil
 	}
 
 	return nil, fmt.Errorf(userKeyError)
 }
 
-// DeleteAgent DELETE /api/v1/agents/{id}
-// https://api.kubiya.ai/api/v1/agents/01b81e08-17eb-4a3e-b0c6-6a48b0f2fad0
-func (c *Client) DeleteAgent(id string) error {
-	m := "DELETE"
-	t := "%s/api/v1/agents/%s?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host, id)
+func (c *Client) queryParams(uri string) string {
+	if len(c.organization) >= 1 && len(c.email) >= 1 {
+		t := "%s?organization=%s&email=%s"
+		return fmt.Sprintf(t, uri, c.organization, c.email)
+	}
 
+	return uri
+}
+
+func (c *Client) downloadFile(uri, path string) error {
+	m := "GET"
 	req, err := http.NewRequest(m, uri, nil)
 	if err != nil || req == nil {
 		if err != nil {
 			return err
 		}
-
 		return fmt.Errorf("failed to create *http.Request")
 	}
 
-	if _, err = c.doBytesHttpRequest(req); err != nil {
+	resp, err := c.doHttpRequest(req)
+	if err != nil || resp == nil {
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("create runner response is empty")
+	}
+	defer closeBody(resp.Body)
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// GetAgents GET /api/v1/agents
-func (c *Client) GetAgents() ([]*Agent, error) {
-	m := "GET"
-	t := "%s/api/v1/agents?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host)
-
-	req, err := http.NewRequest(m, uri, nil)
-	if err != nil || req == nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("failed to create *http.Request")
-	}
-
-	body, err := c.doBytesHttpRequest(req)
-	if err != nil {
-		return nil, err
-	}
-
-	var result []*Agent
-	if err = json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-// DeleteRunner DELETE /api/v1/runners/{name}
-func (c *Client) DeleteRunner(name string) error {
-	m := "DELETE"
-	t := "%s/api/v1/runners/%s?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host, name)
-
-	req, err := http.NewRequest(m, uri, nil)
-	if err != nil || req == nil {
-		if err != nil {
-			return err
-		}
-
-		return fmt.Errorf("failed to create *http.Request")
-	}
-
-	if _, err = c.doBytesHttpRequest(req); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// GetAgentById GET /api/v1/agents/{id}
-func (c *Client) GetAgentById(id string) (*Agent, error) {
-	m := "GET"
-	t := "%s/api/v1/agents/%s?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host, id)
-
-	req, err := http.NewRequest(m, uri, nil)
-	if err != nil || req == nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("failed to create *http.Request")
-	}
-
-	body, err := c.doBytesHttpRequest(req)
-	if err != nil {
-		return nil, err
-	}
-
-	var result *Agent
-	if err = json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-// CreateAgent POST /api/v1/agents
-func (c *Client) CreateAgent(agent *Agent) (*Agent, error) {
-	m := "POST"
-	t := "%s/api/v1/agents?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host)
-
-	payload, err := toJson(agent)
-	if err != nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("agent is nil")
-	}
-
-	req, err := http.NewRequest(m, uri, payload)
-	if err != nil || req == nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("failed to create *http.Request")
-	}
-
-	body, err := c.doBytesHttpRequest(req)
-	if err != nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("create agent response is empty")
-	}
-
-	var result Agent
-	if err = json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-
-	return &result, nil
-}
-
-// CreateRunner POST /api/v1/runners/{name}
-func (c *Client) CreateRunner(name string) (*Runner, error) {
-	m := "POST"
-	t := "%s/api/v1/runners/%s?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host, name)
-
-	req, err := http.NewRequest(m, uri, nil)
-	if err != nil || req == nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("failed to create *http.Request")
-	}
-
-	body, err := c.doBytesHttpRequest(req)
-	if err != nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("create runner response is empty")
-	}
-
-	var result Runner
-	if err = json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-
-	return &result, err
-}
-
-// GetRunnerByName GET /api/v1/runners/{name}
-func (c *Client) GetRunnerByName(name string) (*Runner, error) {
-	m := "GET"
-	t := "%s/api/v1/runners/%s?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host, name)
-
-	req, err := http.NewRequest(m, uri, nil)
-	if err != nil || req == nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("failed to create *http.Request")
-	}
-
-	body, err := c.doBytesHttpRequest(req)
-	if err != nil {
-		return nil, err
-	}
-
-	var result Runner
-	if err = json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-
-	return &result, err
-}
-
-// UpdateAgent PUT /api/v1/agents/{id}
-func (c *Client) UpdateAgent(id string, agent *Agent) (*Agent, error) {
-	m := "PUT"
-	t := "%s/api/v1/agents/%s?organization=kubiya-ai&email=mevrat.avraham@kubiya.ai"
-	uri := fmt.Sprintf(t, c.host, id)
-
-	payload, err := toJson(agent)
-	if err != nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("agent is nil")
-	}
-
-	req, err := http.NewRequest(m, uri, payload)
-	if err != nil || req == nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("failed to create *http.Request")
-	}
-
-	body, err := c.doBytesHttpRequest(req)
-	if err != nil {
-		if err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("update agent response is empty")
-	}
-
-	var result Agent
-	if err = json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-
-	return &result, nil
-}
-
-func (c *Client) doBytesHttpRequest(request *http.Request) ([]byte, error) {
-	res, err := c.doHttpRequest(request)
+func (c *Client) doBytesHttpRequest(r *http.Request) ([]byte, error) {
+	res, err := c.doHttpRequest(r)
 	if err != nil {
 		return nil, err
 	}
@@ -285,14 +98,25 @@ func (c *Client) doBytesHttpRequest(request *http.Request) ([]byte, error) {
 	return body, err
 }
 
-func (c *Client) doHttpRequest(request *http.Request) (*http.Response, error) {
+func (c *Client) doHttpRequest(r *http.Request) (*http.Response, error) {
 	const (
-		t          = "ApiKey %s"
-		authHeader = "Authorization"
+		t = "%s %s"
+		a = "ApiKey"
+		b = "UserKey"
 	)
 
-	header := fmt.Sprintf(t, c.userKey)
-	request.Header.Set(authHeader, header)
+	header := fmt.Sprintf(t, b, c.userKey)
+	if len(c.email) >= 1 && len(c.organization) >= 1 {
+		header = fmt.Sprintf(t, a, c.userKey)
+	}
 
-	return c.client.Do(request)
+	r.Header.Set("Authorization", header)
+
+	return c.client.Do(r)
+}
+
+func (c *Client) doReaderHttpRequest(r *http.Request) (io.Reader, error) {
+	body, err := c.doBytesHttpRequest(r)
+
+	return bytes.NewReader(body), err
 }
