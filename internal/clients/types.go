a87a71fb9a7bc232deaf919a7b4c2cd4455b9a28
mevrat.avraham <mevrat.avraham@kubiya.ai>
2024-05-05 19:49:47 +0300

    * cleanup: remove unused code

--- internal/clients/types.go
+++ internal/clients/types.go
@@ -1,34 +1,51 @@
 package clients
 
-type Agent struct {
-	Name                 string            `json:"name"`
-	Uuid                 string            `json:"uuid"`
-	Email                string            `json:"email"`
-	Image                string            `json:"image"`
-	Links                []string          `json:"links"`
-	Owners               []string          `json:"owners"`
-	Runners              []string          `json:"runners"`
-	Secrets              []string          `json:"secrets"`
-	Starters             []string          `json:"starters"`
-	Metadata             *Metadata         `json:"metadata"`
-	LlmModel             string            `json:"llm_model"`
-	Description          string            `json:"description"`
-	Integrations         []string          `json:"integrations"`
-	Organization         string            `json:"organization"`
-	AllowedUsers         []string          `json:"allowed_users"`
-	AllowedGroups        []string          `json:"allowed_groups"`
-	AiInstructions       string            `json:"ai_instructions"`
-	EnvironmentVariables map[string]string `json:"environment_variables"`
-}
-
 type Runner struct {
 	Url  string `json:"url"`
 	Name string `json:"name"`
 }
 
-type Metadata struct {
-	CreatedAt       string `json:"created_at"`
-	LastUpdated     string `json:"last_updated"`
-	UserCreated     string `json:"user_created"`
-	UserLastUpdated string `json:"user_last_updated"`
+type user struct {
+	Roles      []any    `json:"roles"`
+	UUID       string   `json:"uuid"`
+	Name       string   `json:"name"`
+	Email      string   `json:"email"`
+	Image      string   `json:"image"`
+	Groups     []string `json:"groups"`
+	Status     bool     `json:"status"`
+	CreateAt   string   `json:"create_at"`
+	UserStatus string   `json:"user_status"`
+	InviteLink string   `json:"invite_link"`
+}
+
+type group struct {
+	UUID        string   `json:"uuid"`
+	Name        string   `json:"name"`
+	Roles       []string `json:"roles"`
+	System      bool     `json:"system"`
+	CreateAt    string   `json:"create_at"`
+	Description string   `json:"description"`
+}
+
+type secret struct {
+	Name        string `json:"secret_name"`
+	CreatedBy   string `json:"created_by"`
+	CreatedAt   string `json:"created_at"`
+	Description string `json:"description"`
+}
+
+type integration struct {
+	Id   string `json:"uuid"`
+	Name string `json:"name"`
+}
+
+type state struct {
+	//self         *user
+	users        []*user
+	agents       []*agent
+	groups       []*group
+	runners      []*runner
+	secrets      []*secret
+	webhooks     []*webhook
+	integrations []*integration
 }
