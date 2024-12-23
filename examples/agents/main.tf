9e1ba89bc297955daf0f67f6bcd1313eef28a089
mevrat.avraham <mevrat.avraham@kubiya.ai>
2024-12-23 15:46:46 +0200

    Add Terraform examples for Kubiya resources
    This commit introduces example configurations for various Kubiya resources, including knowledge bases, integrations, agents, webhooks, and sources. Each example demonstrates the use of the Kubiya provider, required fields, and outputs to guide users in integrating these resources into their Terraform configurations.

---
+++ examples/agents/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    kubiya = {
+      source = "kubiya-terraform/kubiya"
+      #       source = "hashicorp.com/edu/kubiya"
+    }
+  }
+}
+
+provider "kubiya" {
+  //Your Kubiya API Key will be taken from the
+  //environment variable KUBIYA_API_KEY
+  //To set the key, please use export KUBIYA_API_KEY="YOUR_API_KEY"
+}
+
+resource "kubiya_agent" "agent" {
+
+  //Mandatory Fields
+  name = "Mevrat-Camel-Case"             //String
+  runner = "runnerv2-5-vcluster"           //String
+  description  = "This agent can perform various Kubernetes tasks using kubectl"
+  instructions = "You are an AI agent specialized in managing Kubernetes clusters using kubectl. Your tasks include monitoring pod statuses, scaling deployments, updating container images, and reporting issues to JIRA."
+
+  //Optional fields, String
+  #   model = "azure/gpt-4"  // If not provided, Defaults to "azure/gpt-4"
+  //If not provided, Defaults to "ghcr.io/kubiyabot/kubiya-agent:stable"
+  #   image = "ghcr.io/kubiyabot/kubiya-agent:stable"
+
+  //Optional Fields (omitting will retain the current values):
+  //Arrays
+  integrations = ["Github"]
+  users = ["mevrat.avraham@kubiya.ai"]
+  groups = ["Admin"]
+}
+
+output "agent" {
+  value = kubiya_agent.agent
+}
