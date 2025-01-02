9dbfe8b8f559ae7757a7d3df40846b877714d3f3
Akiva Bloch <akiva@kubiya.ai>
2025-01-02 20:33:03 +0200

    implement the secret resource

--- internal/entities/secret.go
+++ internal/entities/secret.go
@@ -7,20 +7,20 @@ import (
 
 type SecretModel struct {
 	Name        types.String `tfsdk:"name"`
-	Value       types.String `tfsdk:"name"`
+	Value       types.String `tfsdk:"value"`
+	Description types.String `tfsdk:"description"`
 	CreatedAt   types.String `tfsdk:"created_at"`
 	CreatedBy   types.String `tfsdk:"created_by"`
-	Description types.String `tfsdk:"description"`
 }
 
 func SecretSchema() schema.Schema {
 	return schema.Schema{
 		Attributes: map[string]schema.Attribute{
+			"name":        schema.StringAttribute{Required: true},
+			"value":       schema.StringAttribute{Required: true, Sensitive: true},
+			"description": schema.StringAttribute{Optional: true},
 			"created_at":  schema.StringAttribute{Computed: true},
 			"created_by":  schema.StringAttribute{Computed: true},
-			"name":        schema.StringAttribute{Required: true},
-			"value":       schema.StringAttribute{Required: true},
-			"description": schema.StringAttribute{Optional: true},
 		},
 	}
 }
