e9310bee760a0092b8620f819c1f04af6c47a4e6
mevrat.avraham <mevrat.avraham@kubiya.ai>
2025-05-04 14:39:21 +0300

    Refactor dynamic config initialization for source entities
    Ensure `DynamicConfig` defaults to an empty JSON object when nil. This prevents potential null pointer issues and standardizes entity initialization across all source-related operations.

--- examples/sources/inline_source/main.tf
+++ examples/sources/inline_source/main.tf
@@ -13,33 +13,194 @@ resource "kubiya_inline_source" "hello_world_tool" {
   name   = "mevrat_raz_tool"
   runner = "core-testing-1"
 
-  tools = jsonencode([
+  workflows = jsondecode([
     {
-      name        = "hello_world_tool update"
-      description = "A simple tool that prints 'Hello World' to the console. update"
-      image       = "python:3.9"
-      content     = "print('Hello World update') update"
-      type        = ""
+      file        = "File"
+      name        = "Name"
+      group       = "Group"
+      description = "Description"
+      tags = ["Tags"]
+      schedule = [
+        {
+          expression = "Schedule"
+        }
+      ]
+      stop_schedule = [
+        {
+          expression = "StopSchedule"
+        }
+      ]
+      restart_schedule = [
+        {
+          expression = "RestartSchedule"
+        }
+      ]
+      skip_if_successful = false
+      env = ["Env"]
+      dotenv = ["Dotenv"]
+      default_params     = "DefaultParams"
+      params = [
+        {
+          key = "Params"
+        }
+      ]
+      secrets = ["Secrets"]
+      entrypoint        = "Entrypoint"
+      state             = "State"
+      log_dir           = "LogDir"
+      timeout           = 0
+      delay             = 0
+      restart_wait      = 0
+      max_active_runs   = 0
+      max_clean_up_time = 0
+      steps = [
+        {
+          name        = "Name"
+          description = "Description"
+          icon        = "Icon"
+          label       = "Label"
+          next_steps = ["step-2"]
+          conditions = [
+            {
+              condition = "2025-05-04T12:32:20.161864+03:00"
+            }
+          ]
+          command = "Command"
+          dir     = "Dir"
+          env = ["env1=value1", "env2=value2"]
+          args = {
+            arg1 = "value1"
+          }
+          depends_on = ["DependsOn-1"]
+          timeout      = 0
+          retry        = 0
+          retry_delay  = 0
+          ignore_error = false
+        }
+      ]
+      preconditions = [
+        {
+          type = "Preconditions"
+        }
+      ]
+      handler_on = {
+        failure = {
+          name        = "Name"
+          description = "Description"
+          icon        = "Icon"
+          label       = "Label"
+          next_steps = ["step-2"]
+          conditions = [
+            {
+              condition = "2025-05-04T12:32:20.161865+03:00"
+            }
+          ]
+          command = "Command"
+          dir     = "Dir"
+          env = ["env1=value1", "env2=value2"]
+          args = {
+            arg1 = "value1"
+          }
+          depends_on = ["DependsOn-1"]
+          timeout      = 0
+          retry        = 0
+          retry_delay  = 0
+          ignore_error = false
+        }
+        success = {
+          name        = "Name"
+          description = "Description"
+          icon        = "Icon"
+          label       = "Label"
+          next_steps = ["step-2"]
+          conditions = [
+            {
+              condition = "2025-05-04T12:32:20.161865+03:00"
+            }
+          ]
+          command = "Command"
+          dir     = "Dir"
+          env = ["env1=value1", "env2=value2"]
+          args = {
+            arg1 = "value1"
+          }
+          depends_on = ["DependsOn-1"]
+          timeout      = 0
+          retry        = 0
+          retry_delay  = 0
+          ignore_error = false
+        }
+        cancel = {
+          name        = "Name"
+          description = "Description"
+          icon        = "Icon"
+          label       = "Label"
+          next_steps = ["step-2"]
+          conditions = [
+            {
+              condition = "2025-05-04T12:32:20.161865+03:00"
+            }
+          ]
+          command = "Command"
+          dir     = "Dir"
+          env = ["env1=value1", "env2=value2"]
+          args = {
+            arg1 = "value1"
+          }
+          depends_on = ["DependsOn-1"]
+          timeout      = 0
+          retry        = 0
+          retry_delay  = 0
+          ignore_error = false
+        }
+        exit = {
+          name        = "Name"
+          description = "Description"
+          icon        = "Icon"
+          label       = "Label"
+          next_steps = ["step-2"]
+          conditions = [
+            {
+              condition = "2025-05-04T12:32:20.161866+03:00"
+            }
+          ]
+          command = "Command"
+          dir     = "Dir"
+          env = ["env1=value1", "env2=value2"]
+          args = {
+            arg1 = "value1"
+          }
+          depends_on = ["DependsOn-1"]
+          timeout      = 0
+          retry        = 0
+          retry_delay  = 0
+          ignore_error = false
+        }
+      }
+      smtp = {
+        host     = "Host"
+        port     = "Port"
+        username = "Username"
+        password = "Password"
+      }
+      error_mail = {
+        from        = "From"
+        to          = "To"
+        prefix      = "Prefix"
+        attach_logs = false
+      }
+      info_mail = {
+        from        = "From"
+        to          = "To"
+        prefix      = "Prefix"
+        attach_logs = false
+      }
+      mail_on = {
+        failure = true
+        success = false
+      }
+      hist_retention_days = 0
     }
   ])
-}
-
-resource "kubiya_agent" "helloworld_teammate" {
-  name          = "mevrat_raz_teammate"
-  description   = "HelloWorldTeammate is designed to interact with users and execute a simple 'Hello World' tool. This teammate requires minimal permissions and serves as a basic example of Kubiya's capabilities."
-  image         = "ghcr.io/kubiyabot/kubiya-agent:stable"
-  model         = "azure/gpt-4"
-  runner        = "core-testing-1"
-  is_debug_mode = false
-
-  # environment_variables = {}
-  # integrations          = []
-  # links                 = []
-  # groups                = []
-  # secrets               = []
-  # sources               = []
-  # tool_sources          = []
-  # users                 = []
-  instructions = ""
 }
 
