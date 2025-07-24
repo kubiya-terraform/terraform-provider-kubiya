725fc475cc708ecd17d8a8a1afee136b36396aad
mevrat.avraham <mevrat.avraham@kubiya.ai>
2025-07-24 11:44:47 +0300

    Refactor inline source example for updated resource definitions
    Simplified and updated resource configuration by renaming resources, modifying runners, and adding outputs for tools and workflows. Enhanced clarity in tool definitions and streamlined workflow implementation.

--- examples/sources/inline_source/main.tf
+++ examples/sources/inline_source/main.tf
@@ -9,11 +9,28 @@ terraform {
 
 provider "kubiya" {}
 
-resource "kubiya_inline_source" "hello_world_tool" {
-  name   = "integration_test_tool"
-  runner = ""
+resource "kubiya_inline_source" "tools_source" {
+  name   = "tools_source"
+  runner = "core-testing-2"
 
-  # tools = jsonencode([])
+  tools = jsonencode([
+    {
+      name        = "hello_world_tool update"
+      description = "A simple tool that prints 'Hello World' to the console. update"
+      image       = "python:3.9"
+      content     = "print('Hello World update') update"
+      type        = ""
+    }
+  ])
+}
+
+output "tools" {
+  value = kubiya_inline_source.tools_source
+}
+
+resource "kubiya_inline_source" "workflow_source" {
+  name   = "workflow_source"
+  runner = "core-testing-2"
 
   workflows = jsonencode([
     {
@@ -100,25 +117,7 @@ resource "kubiya_inline_source" "hello_world_tool" {
   ])
 }
 
-resource "kubiya_agent" "helloworld_teammate" {
-  name          = "integration_test_teammate"
-  description   = "HelloWorldTeammate is designed to interact with users and execute a simple 'Hello World' tool. This teammate requires minimal permissions and serves as a basic example of Kubiya's capabilities."
-  image         = "ghcr.io/kubiyabot/kubiya-agent:stable"
-  model         = "azure/gpt-4o"
-  runner        = "core-testing-1"
-  is_debug_mode = false
-
-  # environment_variables = {}
-  # integrations          = []
-  # links                 = []
-  # groups                = []
-  # secrets               = []
-  # sources               = []
-  # tool_sources          = []
-  # users                 = []
-  instructions = ""
+output "workflows" {
+  value = kubiya_inline_source.workflow_source
 }
 
-output "output" {
-  value = kubiya_inline_source.hello_world_tool
-}
